7d61a8df4d80c103198cb866352a962a79b7ea87
devlabs <ykaidong@126.com>
2021-10-09 17:08:15 +0800

    optimize

--- hel.ps1
+++ hel.ps1
@@ -6,9 +6,7 @@
 # @bref     此脚本运行于Windows平台上的PowerShell,  在运行脚本前需要在PowerShell中
 #           执行 "Set-ExecutionPolicy RemoteSigned" 以更改脚本执行策略
 #           此脚本用于将当前文件中所有符合条件的视频文件(后缀名位于 $suffixs 中)
-#           转码为Apple prores编码的低分辨率文件以便于剪辑
-#           所有符合条件的文件将会被转码到 ./$destdir 文件夹中
-#           并且保持原来的文件夹结构
+#           使用FFMpeg重新编码并且保持原来的文件夹结构
 # ----------------------------------------------------------------------------
 # Change Logs:
 # Date          Version     Author          Notes
@@ -20,8 +18,8 @@
 #
 # ----------------------------------------------------------------------------
 
-# 以下数组中, [0]对应proxy, [1]对应resize
-$destdir = "proxy", "resize"     
+# 以下数组中, [0]对应ProRes, [1]对应H264
+$destdir = "ProRes", "H264"     
 $codecs = "prores_ks -profile:v standard -pix_fmt yuv422p10le", "libx264" 
 $resolutions = @(
     @{"name" = "HD";      "content" = "1280x720"},
@@ -30,8 +28,8 @@ $resolutions = @(
 
 $suffixs = "*.mov", "*.mp4", "*.mxf", "*.avi"
 
-# 忽略的文件夹, 必须包含$destdir, 以免重复转换
-$exclude = $destdir + ("04_视觉效果", "07_导出")
+# 需要忽略的文件夹, 所有目标文件夹会被自动忽略掉
+$exclude = "04_视觉效果", "07_导出"
 
 # ----------------------------------------------------------------------------
 
@@ -55,13 +53,13 @@ $title = @"
 File name: hel.ps1
   Version: V0.21
      Date: 2021-10-09
- Descript: 此脚本用于将当前目录中符合条件的视频文件转码为代理文件(Apple PreRes)
-           或小分辨率格式(H.264), 存放代理文件的目标目录为./$($destdir[0]), 存放小分辨率
+ Descript: 此脚本用于将当前目录中符合条件的视频文件进行转码(PreRes or H264)
+           存放ProRes文件的目标目录为./$($destdir[0]), 存放H.264
            的目标文件夹为./$($destdir[1]), 且在目标文件夹中保持源目录结构.
 -------------------------------------------------------------------------------
 
-1. 转换为代理文件
-2. 转换为小分辨率文件
+1. 使用ProRes编码
+2. 使用H.264编码
 
 "@
 
@@ -71,7 +69,7 @@ Write-Host $title
 
 # 功能选择
 while ((1, 2) -notcontains $inputfun) {
-    $inputfun = Read-Host "请选择要执行的操作(1-2)"
+    $inputfun = Read-Host "请选择目标编码(1-2)"
 }
 # 匹配数组(数组从0开始)
 $inputfun = $inputfun - 1
@@ -82,12 +80,16 @@ $n = $resolutions.Length
 # 分辨率选择
 Write-Host ""
 for ($i = 0; $i -le $n-1; $i++) {
-    Write-Host (-join(($i + 1), ". ", $resolutions[$i].content))
+    Write-Host (-join(
+            ($i + 1), 
+            ". $($resolutions[$i].content)",
+            "($($resolutions[$i].name))"
+    ))
 }
 
 Write-Host ""
 while ((1..$n) -notcontains $inputres) {
-    $inputres = Read-Host "请选择分辨率(1-$n)"
+    $inputres = Read-Host "请选择目标分辨率(1-$n)"
 }
 $inputres = $inputres - 1
 
@@ -96,13 +98,26 @@ $inputres = $inputres - 1
 # 脚本运行的目录
 $location = (Get-Location).Path
 
+# 目标根目录
+$destroot = (-join(
+        "\$($destdir[$inputfun])", 
+        "_", 
+        "$($resolutions[$inputres].name)"
+))
+
+# 排除所有目标目录
+$excludedest = @()
+foreach ($d in $destdir) {
+    foreach ($r in $resolutions) {
+        $excludedest += (-join($d, "_", $r.name))
+    }
+}
+$exclude += $excludedest
+
 # 获取 $item 在 目标目录中对应的路径
 function getproxy($item) 
 {
-    Write-Host ($item.FullName.Replace($location, (-join(
-                    $location, "\$($destdir[$inputfun])", "\$($resolutions[$inputres].name)"))))
-    return ($item.FullName.Replace($location, (-join(
-                    $location, "\$($destdir[$inputfun])", "\$($resolutions[$inputres].name)"))))
+    return ($item.FullName.Replace($location, (-join($location, $destroot))))
 }
 
 
@@ -146,8 +161,9 @@ function traverse($set)                                                     # 递
 }
 
 
-if (-not (Test-Path $destdir[$inputfun])) {                                   # 如果目标路径不存在
-    New-Item -ItemType Directory $destdir[$inputfun]                          # 创建之
+# 用于处理项目根目录下有文件的情况
+if (-not (Test-Path $destroot)) {                                   # 如果目标根路径不存在
+    New-Item -ItemType Directory $destroot                          # 创建之
 }
 
 # 获取当前目录下除了代理文件夹和排除文件夹之外的所有条目
